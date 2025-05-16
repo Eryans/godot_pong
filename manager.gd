7dc9f40132b48725f7c912d50c575bf4fde95338
Julio Punkito <julesnoir@hotmail.fr>
2025-05-16 13:09:08 +0200

    made basic AI

--- manager.gd
+++ manager.gd
@@ -2,10 +2,14 @@ extends Node
 
 @export var player: Paddle;
 @export var ai_paddle: Paddle;
+@export var ball: Ball;
+@export var ai_speed: float = 2;
 
 func _ready() -> void:
 	pass
 
 func _physics_process(_delta: float) -> void:
 	player.direction = Input.get_axis("up", "down")
-	ai_paddle.direction = Input.get_axis("ui_up", "ui_down")
+	# ai_paddle.direction = Input.get_axis("ui_up", "ui_down")
+	var ball_to_paddle_distance = clamp(ball.global_position.z - ai_paddle.global_position.z, -1, 1)
+	ai_paddle.direction = ball_to_paddle_distance * ai_speed;
