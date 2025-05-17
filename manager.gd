92c14ce5a833431aad2898d935ad22782619c024
Julio Punkito <julesnoir@hotmail.fr>
2025-05-17 15:16:56 +0200

    refactor: ♻️ refactored

--- manager.gd
+++ manager.gd
@@ -12,31 +12,7 @@ func _ready() -> void:
 func _physics_process(_delta: float) -> void:
 	player.direction = Input.get_axis("up", "down")
 	# ai_paddle.direction = Input.get_axis("ui_up", "ui_down")
-
-	var bonus_position = Vector3(0, 0, 5) # position du bonus
-	var paddle_x = ai_paddle.global_position.x
-
-	# 1. Prédire le point d’impact sur l’axe vertical du paddle
-	var impact_point = get_intersection_point(ball.global_position, Vector3(ball.direction.x, 0, ball.direction.y), paddle_x)
-	debug_mesh.global_position = impact_point
-
-	# 2. Vecteur du point d’impact vers le bonus
-	var vector_to_bonus = bonus_position - impact_point
-	var angle_to_bonus = (vector_to_bonus.z) / (vector_to_bonus.x)
-	# 3. Clamp l’angle pour rester dans les limites du paddle
-	var max_angle = deg_to_rad(75)
-	# angle_to_bonus = clamp(angle_to_bonus, -max_angle, max_angle)
-	print(rad_to_deg(angle_to_bonus))
-	# 4. Convertir l’angle en offset (dans l’intervalle [-1, 1])
-	var t = (angle_to_bonus + max_angle) / (2 * max_angle)
-	var offset = t * 2 - 1
-
-	# 5. Appliquer cet offset sur le point d’impact
-	var paddle_half_height = ai_paddle.get_width() / 2.0
-	var target_z = impact_point.z + offset * paddle_half_height
-
-	# 6. Déplacer le paddle vers cette position cible
-	ai_paddle.direction = sign(target_z - ai_paddle.global_position.z)
+	aim_to_target(Vector3(0, 0, 0))
 
 
 func get_intersection_point(ball_pos: Vector3, ball_dir: Vector3, paddle_x: float) -> Vector3:
@@ -45,3 +21,22 @@ func get_intersection_point(ball_pos: Vector3, ball_dir: Vector3, paddle_x: floa
 
 	var t = (paddle_x - ball_pos.x) / ball_dir.x
 	return ball_pos + ball_dir * t
+
+func aim_to_target(target: Vector3) -> void:
+	var paddle_x: float = ai_paddle.global_position.x
+
+	var impact_point: Vector3 = get_intersection_point(ball.global_position, Vector3(ball.direction.x, 0, ball.direction.y), paddle_x)
+	debug_mesh.global_position = impact_point
+
+	var vector_to_bonus: Vector3 = target - impact_point
+	var angle_to_bonus: float = (vector_to_bonus.z) / (vector_to_bonus.x)
+	var max_angle = deg_to_rad(75)
+
+	# Calcul inverse de l'offest (voir ball.gd) pour convertir l’angle en offset)
+	var t: float = (angle_to_bonus + max_angle) / (2 * max_angle)
+	var offset: float = t * 2 - 1
+
+	var paddle_half_height: float = ai_paddle.get_width() / 2.0
+	var target_z: float = impact_point.z + offset * paddle_half_height
+
+	ai_paddle.direction = clamp(target_z - ai_paddle.global_position.z, -1, 1)
