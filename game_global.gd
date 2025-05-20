52e062c22144f64f28d10c527dd59e9ce65d0e44
Julio Punkito <julesnoir@hotmail.fr>
2025-05-20 12:21:41 +0200

    refactor: ♻️ UI handles scores display and goal
    this might be a bit of bad organization

---
+++ game_global.gd
@@ -0,0 +1,4 @@
+extends Node
+
+@onready var player_A_score: int = 0
+@onready var player_B_score: int = 0
---
+++ ui.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@onready var text_player_A_score: RichTextLabel = %ScoreA;
+@onready var text_player_B_score: RichTextLabel = %ScoreB;
+
+func _ready() -> void:
+	EventManager.connect("goal", _on_goal);
+
+
+func _on_goal(goal_hit: EventManager.PlayerGoalEnum) -> void:
+	if (goal_hit == EventManager.PlayerGoalEnum.A):
+		GameGlobal.player_B_score += 1;
+		text_player_B_score.text = str(GameGlobal.player_B_score);
+	else:
+		GameGlobal.player_A_score += 1;
+		text_player_A_score.text = str(GameGlobal.player_A_score);
