1aa293e0f7c9bc5a089cfc704477f825e4131a3a
Julio Punkito <julesnoir@hotmail.fr>
2025-05-24 10:41:17 +0200

    feat: 🍱 added 3d model for bonus

--- entities/bonus/bonus.gd
+++ entities/bonus/bonus.gd
@@ -2,11 +2,33 @@ extends Area3D
 class_name Bonus
 
 @export var bonus_type: BonusManager.BonusTypeEnum
+@export var mesh_list: Dictionary[StringName, Mesh] = {}
+
+@onready var _mesh_instance: MeshInstance3D = $MeshInstance3D
+var t: float = 0
 
 func _ready() -> void:
 	area_entered.connect(_on_area_entered);
-
+	match (bonus_type):
+		BonusManager.BonusTypeEnum.shield:
+			_mesh_instance.mesh = mesh_list["Shield"]
+		BonusManager.BonusTypeEnum.acceleration:
+			_mesh_instance.mesh = mesh_list["Acceleration"]
+		BonusManager.BonusTypeEnum.paddle_slow_debuff:
+			_mesh_instance.mesh = mesh_list["Slow"]
+		BonusManager.BonusTypeEnum.paddle_shrink_debuff:
+			_mesh_instance.mesh = mesh_list["Shrink"]
+		BonusManager.BonusTypeEnum.paddle_grow_buff:
+			_mesh_instance.mesh = mesh_list["Grow"]
+		_:
+			pass
 func _on_area_entered(area: Area3D) -> void:
 	if (area is Ball):
 		BonusManager.bonus_was_hit_emit(self, area.last_hitted_paddle);
 		call_deferred("queue_free");
+
+func _physics_process(delta: float) -> void:
+	t += delta
+	global_position.y = .5 * sin(t * 2) + 1
+	rotation.y += delta
+	print(sin(t))
