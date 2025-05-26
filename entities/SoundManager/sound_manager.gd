6d88fd36a95cc289e6b8ac5499a7063303141718
Julio Punkito <julesnoir@hotmail.fr>
2025-05-26 14:50:52 +0200

    style: 🍱 goal sfx

--- entities/SoundManager/sound_manager.gd
+++ entities/SoundManager/sound_manager.gd
@@ -2,11 +2,13 @@ extends Node
 
 @onready var hit_bonus_sfx: AudioStreamPlayer = %HitBonus
 @onready var hit_wall_sfx: AudioStreamPlayer = %HitWall
+@onready var goal_sfx: AudioStreamPlayer = %Goal
 @onready var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 
 func _ready() -> void:
     BonusManager.bonus_was_hit.connect(_on_bonus_hit)
     EventManager.ball_hitted_wall.connect(_on_ball_hit_wall)
+    EventManager.goal.connect(_on_goal)
 
 func _on_bonus_hit(_b, _c) -> void:
     hit_bonus_sfx.play();
@@ -17,5 +19,8 @@ func _on_ball_hit_wall() -> void:
     hit_wall_sfx.pitch_scale = _get_random_pitch()
     pass
 
+func _on_goal(_arg) -> void:
+    goal_sfx.play();
+
 func _get_random_pitch() -> float:
-    return rng.randf_range(.9, 1)
+    return rng.randf_range(.8, 1.1)
